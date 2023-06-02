2e07b2218a78e24f1ed48ea4301028b03d108e86
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-06-02 19:08:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function convertTextToImage() {
+function convertTextToSVG() {
   const fileInput = document.getElementById("fileInput");
   const file = fileInput.files[0];
   const textarea = document.querySelector("textarea");
@@ -10,16 +10,48 @@ function convertTextToImage() {
     reader.onload = function (e) {
       const text = e.target.result;
       const binary = convertToBinary(text);
-      const image = createImageFromBinary(binary);
-      displayImage(image);
+      const svg = createSVGFromBinary(binary);
+      displaySVG(svg);
     };
 
     reader.readAsText(file);
   } else if (text) {
     const binary = convertToBinary(text);
-    const image = createImageFromBinary(binary);
-    displayImage(image);
+    const svg = createSVGFromBinary(binary);
+    displaySVG(svg);
   }
+}
+
+function createSVGFromBinary(binary) {
+  const svgNS = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(svgNS, "svg");
+  const imageSize = Math.ceil(Math.sqrt(binary.length));
+
+  svg.setAttribute("width", imageSize);
+  svg.setAttribute("height", imageSize);
+
+  for (let i = 0; i < binary.length; i++) {
+    const x = i % imageSize;
+    const y = Math.floor(i / imageSize);
+    const color = binary[i] === "0" ? "red" : "blue";
+
+    const rect = document.createElementNS(svgNS, "rect");
+    rect.setAttribute("x", x);
+    rect.setAttribute("y", y);
+    rect.setAttribute("width", 1);
+    rect.setAttribute("height", 1);
+    rect.setAttribute("fill", color);
+
+    svg.appendChild(rect);
+  }
+
+  return svg;
+}
+
+function displaySVG(svg) {
+  const imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = "";
+  imageContainer.appendChild(svg);
 }
 
 function convertToBinary(text) {
@@ -34,42 +66,44 @@ function convertToBinary(text) {
   return binary;
 }
 
-function createImageFromBinary(binary) {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  const imageSize = Math.ceil(Math.sqrt(binary.length));
-
-  canvas.width = imageSize;
-  canvas.height = imageSize;
-
-  for (let i = 0; i < binary.length; i++) {
-    const x = i % imageSize;
-    const y = Math.floor(i / imageSize);
-    const color = binary[i] === "0" ? "red" : "blue";
-
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, 1, 1);
-  }
-
-  return canvas.toDataURL();
-}
-
-function displayImage(imageData) {
-  const imageContainer = document.getElementById("imageContainer");
-  imageContainer.innerHTML = `<img src="${imageData}" alt="Converted Image">`;
-}
-
 let zoomLevel = 1;
 const imageContainer = document.getElementById("imageContainer");
 
 imageContainer.addEventListener("click", function (event) {
+  const rect = imageContainer.getBoundingClientRect();
+  const offsetX = event.clientX - rect.left;
+  const offsetY = event.clientY - rect.top;
+
   if (event.ctrlKey) {
     // Zoom out when Ctrl + click
-    zoomLevel -= 0.1;
+    zoomLevel = Math.max(zoomLevel - 0.35, 0.1);
   } else {
     // Zoom in when clicked
-    zoomLevel += 0.1;
+    zoomLevel += 0.35;
   }
 
-  imageContainer.style.transform = `scale(${zoomLevel})`;
+  const scale = `scale(${zoomLevel})`;
+  const translate = `translate(${-offsetX}px, ${-offsetY}px)`;
+  imageContainer.style.transformOrigin = "top left";
+  imageContainer.style.transform = `${translate} ${scale}`;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  if (isMobile) {
+    showModal();
+  }
+});
+
+function showModal() {
+  const modalContainer = document.getElementById("modalContainer");
+  modalContainer.style.display = "flex";
+}
+
+function closeModal() {
+  const modalContainer = document.getElementById("modalContainer");
+  modalContainer.style.display = "none";
+}
